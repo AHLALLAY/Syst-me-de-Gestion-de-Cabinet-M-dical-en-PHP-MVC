706860e6f47455ee7a852c6e41dcfc927400968a
Abderrahmane <abderrahmanahlalaly76@gmail.com>
2025-02-05 15:22:47 +0100

    feat: Ajouter la page des rendez-vous

--- views/patiens/Dashboard.php
+++ views/patiens/Dashboard.php
@@ -9,22 +9,24 @@
 </head>
 
 <body class="bg-green-50 min-h-screen p-8">
-    <?php include_once '../layouts/header.php'?>
+    <?php include_once '../layouts/header.php' ?>
     <main class="max-w-7xl mx-auto">
-        <div class="p-8 rounded-full shadow-md border border-green-100">
-            <div class="grid grid-cols-1 md:grid-cols-2 gap-6">
-                <div class="p-6 bg-white rounded-lg shadow-md border border-green-100 hover:shadow-lg hover:border-cyan-500 transition-all cursor-pointer">
-                    <div class="flex items-center justify-center h-full">
-                        <span class="text-xl font-medium text-green-900 hover:text-cyan-600">Display My Appointments</span>
+        <form action="">
+            <div class="p-8 rounded-full shadow-md border border-green-100">
+                <div class="grid grid-cols-1 md:grid-cols-2 gap-6">
+                    <div class="p-6 bg-white rounded-lg shadow-md border border-green-100 hover:shadow-lg hover:border-cyan-500 transition-all cursor-pointer">
+                        <div class="flex items-center justify-center h-full">
+                            <span class="text-xl font-medium text-green-900 hover:text-cyan-600">Display My Appointments</span>
+                        </div>
                     </div>
-                </div>
-                <div class="p-6 bg-white rounded-lg shadow-md border border-green-100 hover:shadow-lg hover:border-cyan-500 transition-all cursor-pointer">
-                    <div class="flex items-center justify-center h-full">
-                        <span class="text-xl font-medium text-green-900 hover:text-cyan-600">Display Doctors</span>
+                    <div class="p-6 bg-white rounded-lg shadow-md border border-green-100 hover:shadow-lg hover:border-cyan-500 transition-all cursor-pointer">
+                        <div class="flex items-center justify-center h-full">
+                            <span class="text-xl font-medium text-green-900 hover:text-cyan-600">Display Doctors</span>
+                        </div>
                     </div>
                 </div>
             </div>
-        </div>
+        </form>
     </main>
 </body>
 
