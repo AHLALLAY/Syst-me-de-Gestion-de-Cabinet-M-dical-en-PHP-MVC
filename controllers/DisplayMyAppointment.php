8fe26584e61eb9c771e03bef9474f6a240e6d0ea
Abderrahmane <abderrahmanahlalaly76@gmail.com>
2025-02-05 19:51:51 +0100

    refac:Renommer le controllers 'DisplayMyAppointment.php'

---
+++ controllers/DisplayMyAppointment.php
@@ -0,0 +1,6 @@
+<?php
+
+require_once $_SERVER['DOCUMENT_ROOT'] . '/views/patients/Appointments.php';
+
+header('location: ').$_SERVER['DOCUMENT_ROOT'] . '/controllers/DisplayMyAppointment.php';
+exit;
