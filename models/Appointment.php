fac9c7ddf99b444801d852bd0ac3766818bedb06
Abderrahmane <abderrahmanahlalaly76@gmail.com>
2025-02-11 23:24:20 +0100

    up: Mise à jourer la fonction 'getAppointmentsByPatient'

--- models/Appointment.php
+++ models/Appointment.php
@@ -26,7 +26,7 @@ class Appointment
         $db = Database::getInstance();
         $conn = $db->getConnection();
 
-        $stmt = $conn->prepare("SELECT a.specialite, a.statut, a.created_at, u1.user_id,
+        $stmt = $conn->prepare("SELECT a.id, a.specialite, a.statut, a.created_at, u1.user_id,
                                         concat(u1.f_name,' ', u1.l_name) as pateint,
                                         concat(u2.f_name,' ', u2.l_name) as doctor
                                         FROM appointment a
