7bd22189fbbd776fcae17e5fee9cb7458ef273cc
Abderrahmane <abderrahmanahlalaly76@gmail.com>
2025-02-11 23:04:04 +0100

    up: Mise à jour la fonction 'getAppointmentsByPatient' dans model 'Appointment'

--- models/Appointment.php
+++ models/Appointment.php
@@ -1,9 +1,11 @@
 <?php
 require_once 'Database.php';
 
-class Appointment{
+class Appointment
+{
 
-    public function addAppointment($patient, $doctor, $specialite){
+    public function addAppointment($patient, $doctor, $specialite)
+    {
         $db = Database::getInstance();
         $conn = $db->getConnection();
 
@@ -14,7 +16,6 @@ class Appointment{
             $stmt->bindParam(':doctor', $doctor);
             $stmt->bindParam(':specialite', $specialite);
             return $stmt->execute();
-    
         } catch (PDOException $e) {
             echo "Erreur lors de la prise du rendez-vous : " . $e->getMessage();
         }
@@ -25,18 +26,20 @@ class Appointment{
         $db = Database::getInstance();
         $conn = $db->getConnection();
 
-        $stmt = $conn->prepare("SELECT a.app_date, u.f_name, u.l_name, u.speciality
-                                FROM appointment a 
-                                JOIN users u ON a.doctor = u.user_id
-                                WHERE patient = :patient");
-        $stmt->bindValue(':patient', $patientId, PDO::PARAM_INT);
+        $stmt = $conn->prepare("SELECT a.specialite, a.statut, a.created_at, u1.user_id,
+                                        concat(u1.f_name,' ', u1.l_name) as pateint,
+                                        concat(u2.f_name,' ', u2.l_name) as doctor
+                                        FROM appointment a
+                                        JOIN users u1 ON a.patient = u1.user_id
+                                        JOIN users u2 ON a.doctor = u2.user_id
+                                        WHERE u1.user_id = :id");
+        $stmt->bindValue(':id', $patientId, PDO::PARAM_INT);
         $stmt->execute();
 
         return $stmt->fetchAll(PDO::FETCH_ASSOC);
     }
 
-    public function getAppointmentsByDoctor($doctorId)
-    {
+    public function getAppointmentsByDoctor($doctorId){
         $db = Database::getInstance();
         $conn = $db->getConnection();
 
